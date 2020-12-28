5eab9455d76b0de20743268459d972c7529a01b0
ronymarry <74005397+ronymarry@users.noreply.github.com>
2020-12-28 20:08:35 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,21 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visiblility=255
+  }
+display()
+{
+  
+  if(this.body.speed<3){
+    super.display();
+  }
+  else{
+    push()
+     World.remove(world,this.body)
+     this.visiblility=this.visiblility-5
+     tint(255,this.visiblility)
+     image(this.image,this.body.position.x,this.body.position.y,50,50)
+     pop()
+  }
+}};
